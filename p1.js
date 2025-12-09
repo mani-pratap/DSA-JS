a8412a86da84b6abe1e93fd9966735f8fcc4221c
Manish Singh <pratapmanish63@gmail.com>
2025-12-09 17:15:45 +0530

    string

---
+++ p1.js
@@ -0,0 +1,13 @@
+// let a = 10;
+// let b = 20;
+// 
+// // a = a+b //30
+// // b = a-b //10
+// // a = a-b // 20
+// 
+// // [a, b] = [b, a]; // array destructuring
+// 
+// 
+// 
+// console.log(a);
+// console.log(b);
